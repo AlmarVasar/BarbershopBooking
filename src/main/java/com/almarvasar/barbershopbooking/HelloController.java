91160f2f1ca800fb5b15e1de73ba4831763adb99
Almar Vasar <almar.vasar@gmail.com>
2023-03-12 14:30:21 +0200

    added more values for choisebox. Added construtor for HelloController. Assigned some logic to Book button

--- src/main/java/com/almarvasar/barbershopbooking/HelloController.java
+++ src/main/java/com/almarvasar/barbershopbooking/HelloController.java
@@ -2,25 +2,53 @@ package com.almarvasar.barbershopbooking;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import org.hibernate.SessionFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
-    @FXML
-    private Label barber;
 
-    @FXML
+    private SessionFactory session;
 
+    //@FXML private DatePicker dateCB;
+    @FXML
     private ChoiceBox<String> barberCB;
+    @FXML
+    private ChoiceBox<String> serviceCB;
+    @FXML
+    private ChoiceBox<String> timeCB;
+    @FXML
+    private Button insertButton;
 
-    private String[] barberNames ={"Funky Hair", "Slick Dude", "Hairy Beard"};
+    //@FXML private Button contactButton;
 
+
+    private String[] barberNames = {"Funky Hair", "Slick Dude", "Hairy Beard"};
+    private String[] service = {"Haircut", "Beard trim", "Haircut and beard trim"};
+    private String[] time = {"09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00",};
+
+
+    public HelloController(SessionFactory session) {
+        this.session = session;
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         barberCB.getItems().addAll(barberNames);
+        serviceCB.getItems().addAll(service);
+        timeCB.getItems().addAll(time);
+
+        this.insertButton.setOnAction(e -> {
+            var ses = session.openSession();
+            var tx = ses.beginTransaction();
+
+            var booking = new Booking();
+            booking.setBarber(String.valueOf(barberCB));
+            ses.save(booking);
+
+            tx.commit();
+            ses.close();
+        });
     }
 }
