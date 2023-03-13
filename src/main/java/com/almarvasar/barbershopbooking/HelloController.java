3ae20d68afef89dfdabf944ba9986bd8d82bd526
Almar Vasar <almar.vasar@gmail.com>
2023-03-13 20:10:12 +0200

    Added methods to save date, barber and service

--- src/main/java/com/almarvasar/barbershopbooking/HelloController.java
+++ src/main/java/com/almarvasar/barbershopbooking/HelloController.java
@@ -12,39 +12,45 @@ public class HelloController implements Initializable {
 
     private SessionFactory session;
 
-    //@FXML private DatePicker dateCB;
+    @FXML private DatePicker dateCB;
     @FXML
     private ChoiceBox<String> barberCB;
     @FXML
     private ChoiceBox<String> serviceCB;
-    @FXML
-    private ChoiceBox<String> timeCB;
+   // @FXML
+    //private ChoiceBox timeCB;
     @FXML
     private Button insertButton;
 
     //@FXML private Button contactButton;
 
-
+//Created arrays for choisebox
     private String[] barberNames = {"Funky Hair", "Slick Dude", "Hairy Beard"};
     private String[] service = {"Haircut", "Beard trim", "Haircut and beard trim"};
-    private String[] time = {"09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00",};
+
+   // private Integer[] time = {09:00, 10:00, 11:00, 12:00, 13:00, 14:00, 15:00, 16:00""};
 
 
     public HelloController(SessionFactory session) {
         this.session = session;
     }
+    //Display my options in my JavaFX
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         barberCB.getItems().addAll(barberNames);
         serviceCB.getItems().addAll(service);
-        timeCB.getItems().addAll(time);
+        //timeCB.getItems().addAll(time);
 
+        //Open mysql and save the selected data in database
         this.insertButton.setOnAction(e -> {
             var ses = session.openSession();
             var tx = ses.beginTransaction();
 
             var booking = new Booking();
-            booking.setBarber(String.valueOf(barberCB));
+            booking.setDate(java.sql.Date.valueOf(dateCB.getValue())); //
+            booking.setBarber(barberCB.getValue());
+            booking.setService(serviceCB.getValue());
+            //booking.setTime((Integer) timeCB.getValue());
             ses.save(booking);
 
             tx.commit();
